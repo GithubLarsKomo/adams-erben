614aa0e14d3d048a542caf9e57f1a7996931b995
GithubLarsKomo <76850395+GithubLarsKomo@users.noreply.github.com>
2026-08-10 03:35:46 +0200

    feat: add verified BRV functional alias hint

--- scripts/lib/lrv-identity.mjs
+++ scripts/lib/lrv-identity.mjs
@@ -2,21 +2,35 @@ export const LRV_IDENTITY_HINTS = new Map([
   ['30010', {
     website: 'https://www.lrvbw.de/',
     contactDomains: ['rudern-bw.de'],
+    functionalLocalParts: [],
+    verificationSource: 'official-lrv-imprint'
+  }],
+  ['30011', {
+    website: 'https://www.ruderverband.de/',
+    contactDomains: ['ruderverband.de'],
+    functionalLocalParts: ['brv-geschaeftsstelle'],
     verificationSource: 'official-lrv-imprint'
   }],
   ['30018', {
     website: 'https://www.lrvn.de/',
     contactDomains: ['lrvn.de'],
+    functionalLocalParts: [],
     verificationSource: 'official-lrv-imprint'
   }],
   ['30019', {
     website: 'https://www.rudern.nrw/',
     contactDomains: ['nwrv.org'],
+    functionalLocalParts: [],
     verificationSource: 'official-lrv-imprint'
   }]
 ]);
 
 export function lrvIdentityHints(record = {}) {
   const drvId = String(record.drvId || record.organizationId || '').trim();
-  return LRV_IDENTITY_HINTS.get(drvId) || { website: '', contactDomains: [], verificationSource: '' };
+  return LRV_IDENTITY_HINTS.get(drvId) || {
+    website: '',
+    contactDomains: [],
+    functionalLocalParts: [],
+    verificationSource: ''
+  };
 }
